6edac257cc34225e30dbd37274e916911c111180
Phat Hong <phat@backbase.com>
2025-07-17 13:11:54 +0700

    Update json data

--- ky-niem-web/src/app/pages/album-anh/album-anh.component.ts
+++ ky-niem-web/src/app/pages/album-anh/album-anh.component.ts
@@ -1,44 +1,41 @@
-import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-album-anh',
-  imports: [
-    ButtonModule,
-    CommonModule
-  ],
+  imports: [CommonModule],
   templateUrl: './album-anh.component.html',
-  styleUrls: [ './album-anh.component.scss' ]
+  styleUrls: ['./album-anh.component.scss']
 })
-export class AlbumAnhComponent {
-  images = [
-    { id: 1, src: 'assets/images/ki-niem/ki-niem-1.jpg', title: 'Chuyến đi đầu tiên', favorite: false },
-    { id: 2, src: 'assets/images/ki-niem/ki-niem-2.jpg', title: 'Kỷ niệm 1 năm', favorite: false },
-    { id: 3, src: 'assets/images/ki-niem/ki-niem-3.jpg', title: 'Sinh nhật đáng nhớ', favorite: false },
-    { id: 4, src: 'assets/images/ki-niem/ki-niem-4.jpg', title: 'Khoảnh khắc lãng mạn', favorite: false },
-    { id: 5, src: 'assets/images/ki-niem/ki-niem-5.jpg', title: 'Kỷ niệm đặc biệt', favorite: false },
-    { id: 6, src: 'assets/images/ki-niem/ki-niem-6.jpg', title: 'Hạnh phúc bên nhau', favorite: false },
-    { id: 7, src: 'assets/images/hinh-cuoi/wedding-1.jpg', title: 'Ảnh cưới tuyệt đẹp', favorite: false },
-    { id: 8, src: 'assets/images/hinh-cuoi/wedding-2.jpg', title: 'Ngày cưới hạnh phúc', favorite: false }
-  ];
+export class AlbumAnhComponent implements OnInit {
+  
+  albumPhotos: any[] = [];
 
-  toggleFavorite(img: any) {
-    img.favorite = !img.favorite;
-    this.saveFavorites();
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadAlbumData();
   }
 
-  saveFavorites() {
-    const favs = this.images.filter(i => i.favorite).map(i => i.id);
-    localStorage.setItem('favoriteImages', JSON.stringify(favs));
+  private loadAlbumData() {
+    this.http.get<any>('assets/data/album-data.json').subscribe({
+      next: (data) => {
+        this.albumPhotos = data.albumPhotos;
+      },
+      error: (error) => {
+        console.error('Error loading album data:', error);
+        this.setFallbackData();
+      }
+    });
   }
 
-  loadFavorites() {
-    const favs = JSON.parse(localStorage.getItem('favoriteImages') || '[]');
-    this.images.forEach(i => i.favorite = favs.includes(i.id));
-  }
-
-  constructor() {
-    this.loadFavorites();
+  private setFallbackData() {
+    this.albumPhotos = [
+      { id: 1, src: 'assets/images/ki-niem/img.png', caption: 'Khoảnh khắc đầu tiên bên nhau' },
+      { id: 2, src: 'assets/images/ki-niem/img_1.png', caption: 'Những ngày đầu hẹn hò' },
+      { id: 3, src: 'assets/images/ki-niem/img_2.png', caption: 'Chuyến du lịch đáng nhớ' },
+      { id: 4, src: 'assets/images/ki-niem/img_3.png', caption: 'Lễ cưới hạnh phúc' }
+    ];
   }
 }
