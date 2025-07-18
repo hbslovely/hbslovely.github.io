6c8c91513e336d7ef778956dc81124d5776dd357
Phat Hong <phat@backbase.com>
2025-07-18 09:12:54 +0700

    Sua mon an yeu thich

--- ky-niem-web/src/app/shared/components/favorite-foods/favorite-foods.component.ts
+++ ky-niem-web/src/app/shared/components/favorite-foods/favorite-foods.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PersonFilterPipe } from '../../pipes/person-filter.pipe';
 
-interface FavoriteFood {
-  dish: string;
+interface Food {
   person: string;
+  dish: string;
   story: string;
   feeling: string;
 }
@@ -11,10 +12,47 @@ interface FavoriteFood {
 @Component({
   selector: 'app-favorite-foods',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PersonFilterPipe],
   templateUrl: './favorite-foods.component.html',
   styleUrls: ['./favorite-foods.component.scss']
 })
 export class FavoriteFoodsComponent {
-  @Input() foods: FavoriteFood[] = [];
+  foods: Food[] = [
+    {
+      person: 'Chồng',
+      dish: 'Bún bò Huế',
+      story: 'Món ăn yêu thích từ thời sinh viên',
+      feeling: 'Nhớ mãi hương vị quê nhà'
+    },
+    {
+      person: 'Chồng',
+      dish: 'Cơm tấm sườn',
+      story: 'Bữa sáng năng lượng cho ngày mới',
+      feeling: 'Hương vị Sài Gòn thân thương'
+    },
+    {
+      person: 'Chồng',
+      dish: 'Bánh canh cua',
+      story: 'Món ngon cuối tuần thư giãn',
+      feeling: 'Ấm bụng, ấm lòng'
+    },
+    {
+      person: 'Vợ',
+      dish: 'Phở gà',
+      story: 'Món ăn truyền thống gia đình',
+      feeling: 'Ấm áp tình thương'
+    },
+    {
+      person: 'Vợ', 
+      dish: 'Bánh mì thịt nướng',
+      story: 'Bữa sáng yêu thích mỗi ngày',
+      feeling: 'Năng lượng tràn đầy'
+    },
+    {
+      person: 'Vợ',
+      dish: 'Bún chả',
+      story: 'Món ngon đậm đà Hà Nội',
+      feeling: 'Hương vị khó quên'
+    }
+  ];
 } 
