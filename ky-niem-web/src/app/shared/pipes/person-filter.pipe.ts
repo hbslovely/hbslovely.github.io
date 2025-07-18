1b866ce0997e578bbdcfba245c68c25b47f1e867
Phat Hong <phat@backbase.com>
2025-07-18 09:25:19 +0700

    Sua mon an yeu thich

---
+++ ky-niem-web/src/app/shared/pipes/person-filter.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+interface Food {
+  person: string;
+  dish: string;
+  story: string;
+  feeling: string;
+}
+
+@Pipe({
+  name: 'filter',
+  standalone: true
+})
+export class PersonFilterPipe implements PipeTransform {
+  transform(foods: Food[] | null, person: string): Food[] {
+    if (!foods) return [];
+    return foods.filter(food => food.person === person);
+  }
+} 
