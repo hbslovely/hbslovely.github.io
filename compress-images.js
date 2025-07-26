7f9dd3fc1f5587235058ca4eaae8775e7b5dfa6d
Phat Hong <phat.hong@hcl.com>
2025-07-27 00:11:07 +0700

    Compress images

--- compress-images.js
+++ compress-images.js
@@ -3,16 +3,42 @@ const fs = require('fs');
 const path = require('path');
 
 const imagesToCompress = [
-    'ky-niem-web/src/assets/images/places/checkin-ba-chua-xu-nui-sam.png',
-    'ky-niem-web/src/assets/images/places/checkin-cau-hien-luong-quang-tri.png',
-    'ky-niem-web/src/assets/images/places/checkin-thanh-co-quang-tri.jpg',
-    'ky-niem-web/src/assets/images/phat/phat-quyen-ben-nhau.png',
-    'ky-niem-web/src/assets/images/places/checkin-dinh-thay-thim.png',
-    'ky-niem-web/src/assets/images/phat/img.png',
-    'ky-niem-web/src/assets/images/places/checkin-cho-dong-xuan-da-lat.png',
-    'ky-niem-web/src/assets/images/places/checkin-chua-thien-mu.jpg',
-    'ky-niem-web/src/assets/images/places/checkin-canh-dong-thang-long-binh-thuan.png',
-    'ky-niem-web/src/assets/images/phat/img_1.png'
+    // Images from ky-niem-web
+    'ky-niem-web/src/assets/images/anh-chu-dao/avatar-couple.png',                    // 5.5MB
+    'ky-niem-web/src/assets/images/anh-chu-dao/avatar-couple-back.png',               // 11MB
+    'ky-niem-web/src/assets/images/mon-qua/mon-qua-thu-3-la-thu-tay-dau-tien-vo-gui-chong.png', // 6.6MB
+    'ky-niem-web/src/assets/images/mon-qua/mon-qua-thu-4-chuyen-du-lich-bac-nam.png', // 9.4MB
+    'ky-niem-web/src/assets/images/mon-qua/mon-qua-thu-2-nhan-doi.png',               // 5.4MB
+    'ky-niem-web/src/assets/images/mon-qua/mon-qua-dau-tien-4-buc-tuong-chu-tieu.png', // 7.5MB
+    'ky-niem-web/src/assets/images/places/checkin-mui-dien-phu-yen.png',              // 5.0MB
+    'ky-niem-web/src/assets/images/gallery/su-an-ui.png',                             // 6.6MB
+    'ky-niem-web/src/assets/images/gallery/nam-tay-yeu-thuong.png',                   // 4.6MB
+    'ky-niem-web/src/assets/images/gallery/nuoc-mat-hanh-phuc.png',                   // 6.3MB
+    'ky-niem-web/src/assets/images/gallery/chuyen-di-dau-tien.png',                   // 6.6MB
+    'ky-niem-web/src/assets/images/gallery/ngay-dau-ben-nhau.png',                    // 5.1MB
+    'ky-niem-web/src/assets/images/gallery/nu-cuoi.png',                              // 8.6MB
+    'ky-niem-web/src/assets/images/gallery/su-ben-nhau-moi-luc.png',                  // 7.5MB
+    'ky-niem-web/src/assets/images/ki-niem/trao-duyen.png',                          // 5.7MB
+    'ky-niem-web/src/assets/images/ki-niem/gap-mat-gia-dinh.png',                    // 5.3MB
+    'ky-niem-web/src/assets/images/ki-niem/sau-rieng-vo-gui-tu-dak-lak_8-2021.png',  // 7.9MB
+    'ky-niem-web/src/assets/images/foods/nem-nuong.png',                              // 7.6MB
+    'ky-niem-web/src/assets/images/hinh-cuoi/dam-cuoi-03-2022.png',                   // 7.7MB
+    'ky-niem-web/src/assets/images/hinh-cuoi/mang-thai.png',                          // 5.5MB
+    'ky-niem-web/src/assets/images/quyen/quyen-portrait.png',                         // 7.6MB
+    'ky-niem-web/src/assets/images/albums/ngay-cuoi_3-2022/10.png',                   // 9.7MB
+    'ky-niem-web/src/assets/images/albums/ngay-cuoi_3-2022/7.png',                    // 6.0MB
+    'ky-niem-web/src/assets/images/albums/ngay-cuoi_3-2022/6.png',                    // 4.7MB
+    'ky-niem-web/src/assets/images/albums/ngay-cuoi_3-2022/2.png',                    // 4.1MB
+    'ky-niem-web/src/assets/images/albums/ngay-cuoi_3-2022/3.png',                    // 5.0MB
+
+    // Images from memories
+    'memories/assets/images/albums/du-lich/4.png',                                     // 9.0MB
+    'memories/assets/images/albums/du-lich/5.png',                                     // 4.8MB
+    'memories/assets/images/albums/du-lich/2.png',                                     // 11MB
+    'memories/assets/images/albums/du-lich/3.png',                                     // 4.9MB
+    'memories/assets/images/albums/du-lich/1.png',                                     // 5.3MB
+    'memories/assets/images/albums/doi-thuong/7.png',                                  // 6.1MB
+    'memories/assets/images/albums/doi-thuong/3.png'                                   // 6.9MB
 ];
 
 async function compressImage(inputPath) {
