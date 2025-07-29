95bd3c986ea95af144a2870c38a6386c4eb12267
Phat Hong <phat@backbase.com>
2025-07-29 14:27:44 +0700

    Update the UI

--- ky-niem-web/src/app/app.module.ts
+++ ky-niem-web/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-import { MemoryPlacesComponent } from './shared/components/memory-places/memory-places.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutes } from './app.routes';
+import { GratitudeMessagesComponent } from './shared/components/gratitude-messages/gratitude-messages.component';
+
+// Import Swiper styles in your styles.scss instead of here
 
 @NgModule({
   declarations: [
     AppComponent,
-    MemoryPlacesComponent
+    GratitudeMessagesComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ky-niem-web/src/app/shared/components/gratitude-messages/gratitude-messages.component.ts
+++ ky-niem-web/src/app/shared/components/gratitude-messages/gratitude-messages.component.ts
@@ -1,16 +1,47 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
+import { Navigation, Pagination, EffectFade } from 'swiper/modules';
 
 @Component({
   selector: 'app-gratitude-messages',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './gratitude-messages.component.html',
-  styleUrls: ['./gratitude-messages.component.scss'],
-  host: {
-    'style': `
-      @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&family=Crimson+Text:ital,wght@0,400;0,700;1,400;1,700&display=swap');
-    `
-  }
+  styleUrls: ['./gratitude-messages.component.scss']
 })
-export class GratitudeMessagesComponent {} 
+export class GratitudeMessagesComponent implements OnInit, AfterViewInit {
+  private swiper: Swiper | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.initSwiper();
+  }
+
+  private initSwiper(): void {
+    this.swiper = new Swiper('.swiper-container', {
+      modules: [Navigation, Pagination, EffectFade],
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      speed: 800,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+    });
+  }
+} 
