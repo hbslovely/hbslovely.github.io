1c8504f23a718c0cb1cb9c5c0286b9524b17f9a9
Phat Hong <phat@backbase.com>
2025-08-06 17:42:15 +0700

    Add missing files

---
+++ ky-niem-web/src/app/shared/components/sweet-moments/sweet-moments.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SweetMoment } from '../../models/profile.model';
+
+@Component({
+  selector: 'app-sweet-moments',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sweet-moments.component.html',
+  styleUrls: ['./sweet-moments.component.scss']
+})
+export class SweetMomentsComponent {
+  @Input() moments: SweetMoment[] = [];
+} 
