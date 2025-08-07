09ea535ae0a6635e392cf255e3eb540c15e98250
Phat Hong <phat.hong@hcl.com>
2025-08-07 11:28:34 +0700

    Update husband data

--- ky-niem-web/src/app/shared/components/profile-detail/profile-detail.component.ts
+++ ky-niem-web/src/app/shared/components/profile-detail/profile-detail.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Profile } from '../../models/profile.model';
 import { SweetMomentsComponent } from '../sweet-moments/sweet-moments.component';
 import { SectionContainerComponent } from '../section-container/section-container.component';
+import { ProfileHeaderComponent } from '../profile-header/profile-header.component';
 
 @Component({
   selector: 'app-profile-detail',
   standalone: true,
-  imports: [CommonModule, SweetMomentsComponent, SectionContainerComponent],
+  imports: [CommonModule, SweetMomentsComponent, SectionContainerComponent, ProfileHeaderComponent],
   templateUrl: './profile-detail.component.html',
   styleUrls: ['./profile-detail.component.scss']
 })
